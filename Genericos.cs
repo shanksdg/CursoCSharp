971df29f8b06b78c448cb5a2582e119fafe26fea
shanksdg <122836280+shanksdg@users.noreply.github.com>
2024-06-14 14:12:37 -0300

    Add files via upload

---
+++ Genericos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CursoCSharp.ClassesEMetodos;
+
+namespace CursoCSharp.TopicosAvançados
+{
+    public class Caixa<T> {
+        T valorPrivado;
+        public T Coisa { get; set; }
+
+        public Caixa(T coisa) {
+            Coisa = coisa;
+            valorPrivado = Coisa;
+        }
+
+        public T metodoGenerico(T valor) {
+            return new Random().Next(0, 2) == 0 ? Coisa : valor;
+        }
+
+        public T Getvalor() {
+            return valorPrivado;
+        }
+
+        class CaixaProduto : Caixa<Produto> {
+            public CaixaProduto() : base(new Produto()) {
+
+            }
+        }
+    }
+    internal class Genericos
+    {
+        public static void Executar() {
+            var caixa1 = new Caixa<int>(1000);
+            Console.WriteLine(caixa1.metodoGenerico(33));
+            Console.WriteLine(caixa1.Coisa.GetType());
+
+            var caixa2 = new Caixa<string>("Construtor");
+            Console.WriteLine(caixa2.metodoGenerico("Método"));
+            Console.WriteLine(caixa2.Coisa.GetType());
+
+            CaixaProduto caixa3 = new CaixaProduto();
+            Console.WriteLine(caixa3.Coisa.GetType().Name);
+        }
+    }
+}
